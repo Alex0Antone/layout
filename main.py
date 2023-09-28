4e7ea8cfe2c71406134a1d684b2972c70f7e5108
s10016635 <alexantone@icloud.com>
2023-09-28 09:33:24 -0500

    Started kivy layout assignment

--- main.py
+++ main.py
@@ -5,7 +5,13 @@ from kivy.uix.stacklayout import StackLayout
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 class GridLayoutExample(GridLayout):
-    pass
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.cols = 3
+        for letter in "0123456789abcdefgh":
+            b = Button(text="Button "+letter.upper(),  background_color=(0, 1, .1))
+            self.add_widget(b)
+
 
 class LayoutAssignment(BoxLayout):
     pass
@@ -14,7 +20,7 @@ class StackLayoutExample(StackLayout):
         super().__init__(**kwargs)
         self.orientation = "lr-tb"
         for letter in "abcdefghijklmnopqrstuvwxyz0123456789":
-            b = Button(text ="Button "+ letter.upper(), size_hint=(.2, .2))
+            b = Button(text ="Button "+ letter.upper())
             self.add_widget(b)
 class BoxLayoutExample(BoxLayout):
     pass
